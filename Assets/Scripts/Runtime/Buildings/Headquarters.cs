d929a32f7ce1075aa4eefac1d36aedb2c56e3ec4
Twaitsy <twaitsy@hotmail.co.uk>
2026-02-15 20:22:43 +0000

    23.a

--- Assets/Scripts/Runtime/Buildings/Headquarters.cs
+++ Assets/Scripts/Runtime/Buildings/Headquarters.cs
@@ -2,12 +2,27 @@
 
 public class Headquarters : Building
 {
+    [Header("Starting Resources")]
+    [SerializeField] private ResourceAmount[] startingResources;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        // Deposit starting resources for this team (assumes one HQ per team; adjust if needed)
+        foreach (var amount in startingResources)
+        {
+            if (amount.resource != null && amount.amount > 0) // Null check since ResourceDefinition is a struct but drawer may leave it default
+            {
+                TeamResources.Instance.Deposit(teamID, amount.resource, amount.amount);
+            }
+        }
+    }
+
     protected override void Die()
     {
         Debug.Log($"Team {teamID} Headquarters destroyed!");
-
         // TODO (later): trigger victory / defeat logic here
-
         base.Die();
     }
 }
